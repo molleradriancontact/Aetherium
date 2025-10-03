c3c93767d04ec2a8d0b8a7e004119d74a40077cc
Adrian <molleradriancontact@gmail.com>
2025-10-03 04:09:57 +0000

    Try fixing this error: `Build Error: Export firebase doesn't exist in ta

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,13 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {firebase} from '@genkit-ai/firebase';
+import {firebase} from '@genkit-ai/firebase/plugin';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
     firebase(),
   ],
-  model: 'googleai/gemini-2.5-flash',
   flowStateStore: 'firebase',
   traceStore: 'firebase',
+  cacheStore: 'firebase',
 });
