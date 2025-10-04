c3754d09de4d970cc928185d709a6e7205dc9210
Adrian <molleradriancontact@gmail.com>
2025-10-04 03:12:38 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import { useFirebase, initiateEmailSignIn, initiateGoogleSignIn } from '@/firebase';
